c182d22b6126df1c3deed9429f1c98b00bd0cc5f
Qianqian Zhou <qqzh1214@gmail.com>
2024-12-01 22:58:02 +0100

    Create order using the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,35 +1,31 @@
-import { renderOrderSummary } from "./checkout/orderSummary.js";
-import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadFromStorage } from "../data/cart.js";
-import { loadProducts, loadProductsFetch } from "../data/products.js";
-import { loadCart } from "../data/cart.js";
+import {renderOrderSummary} from './checkout/orderSummary.js';
+import {renderPaymentSummary} from './checkout/paymentSummary.js';
+import {loadProducts, loadProductsFetch} from '../data/products.js';
+import {loadCart} from '../data/cart.js';
 // import '../data/cart-class.js';
-//import '../data/backend-practice.js';
+// import '../data/backend-practice.js';
 
 async function loadPage() {
-
   try {
-    //throw 'error1';
+    // throw 'error1';
 
     await loadProductsFetch();
 
     const value = await new Promise((resolve, reject) => {
-      //throw 'error2';
+      // throw 'error2';
       loadCart(() => {
-        //reject('error3');
-        resolve(value3);
+        // reject('error3');
+        resolve('value3');
       });
     });
 
   } catch (error) {
-    console.log('Unexpceted error. Please try again later');
+    console.log('Unexpected error. Please try again later.');
   }
 
   renderOrderSummary();
   renderPaymentSummary();
-  
 }
-
 loadPage();
 
 /*
